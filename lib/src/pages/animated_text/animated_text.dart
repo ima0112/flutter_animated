11c4dbf14539444dbcfa43c4b389d7222f52bde3
ima0112 <imandracardenas0@gmail.com>
2023-11-10 10:32:04 -0500

    Implementando el animated text

---
+++ lib/src/pages/animated_text/animated_text.dart
@@ -0,0 +1,70 @@
+import 'dart:math';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+
+import '../../utils/constants.dart';
+
+class AnimatedTextPage extends StatefulWidget {
+  const AnimatedTextPage({super.key});
+
+  @override
+  State<AnimatedTextPage> createState() => _AnimatedTextPageState();
+}
+
+class _AnimatedTextPageState extends State<AnimatedTextPage> {
+  bool value = true;
+
+  @override
+  void initState() {
+    value = Random().nextBool();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Text'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          height: 300,
+          width: double.infinity,
+          color: Colors.redAccent,
+          child: value ? buildWavyAnimation() : buildColorizeAnimation(),
+        ),
+      ),
+    );
+  }
+
+  Widget buildWavyAnimation() {
+    return Center(
+      child: AnimatedTextKit(
+        animatedTexts: [WavyAnimatedText('Hello World')],
+        repeatForever: true,
+      ),
+    );
+  }
+
+  Widget buildColorizeAnimation() {
+    return Center(
+      child: AnimatedTextKit(
+        animatedTexts: [
+          ColorizeAnimatedText(
+            'APPLE',
+            textStyle: colorizeTextStyle,
+            colors: colorizeColors,
+          ),
+          ColorizeAnimatedText(
+            'SAMSUNG',
+            textStyle: colorizeTextStyle,
+            colors: colorizeColors,
+          ),
+        ],
+        repeatForever: true,
+      ),
+    );
+  }
+}
---
+++ lib/src/utils/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const colorizeColors = [
+  Colors.purple,
+  Colors.blue,
+  Colors.yellow,
+  Colors.red,
+];
+
+const colorizeTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
