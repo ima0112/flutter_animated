343e131f65d9244d11ef62e8c77ac8fbd518cb38
ima0112 <imandracardenas0@gmail.com>
2023-11-10 14:59:48 -0500

    Agregando un asset de rive

---
+++ lib/src/utils/assets.dart
@@ -0,0 +1,8 @@
+///This class contains all assets of app
+class AppAsset {
+  AppAsset._();
+
+  //Rive Assets
+  static const String splashScreen = 'assets/rive/2063-4080-flutter-puzzle-hack-project.riv';
+  
+}
