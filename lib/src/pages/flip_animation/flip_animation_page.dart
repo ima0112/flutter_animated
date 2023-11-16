d4cc80cbf3661cc746cb0b681d649823d45c3cef
ima0112 <imandracardenas0@gmail.com>
2023-11-16 13:36:39 -0500

    Add FlipAnimationPage to route

---
+++ lib/src/pages/flip_animation/flip_animation_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlipAnimationPage extends StatelessWidget {
+  const FlipAnimationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
